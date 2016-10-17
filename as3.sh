739b9bee52fd9a2b005e68f534496bc7ee0a61fa
AdeleMcCallum <12adm10@queensu.ca>
2016-10-17 18:41:00 -0400

    Update as3.sh

--- as3.sh
+++ as3.sh
@@ -4,4 +4,46 @@
 #Adele McCallum 10093867\
 #Dean Wilkins-Reeves 10176758\
 # git clone https://github.com/kateduquesnay/cisc220_1.git cisc220_1\
-#Question 4\
+#Question 3\
+
+
+#!/bin/bash
+
+#print beginning shit
+#while:
+#print current race positions, (~ before, spaces after)
+#check if car reached 40 picks
+#if yes, print player wins, break
+#single character input from keyboard
+#if input is 1/2/3/ car 1/2/3 moves on step 
+#clear command
+
+
+#variables needed: 
+  #car 1, car 1's position
+  #car 2, car 2's position
+  #car 3, car 3's position
+  #input?
+  
+#while (car1pos != 40 && car2pos != 40 && car3pos != 40):
+
+  #if (number 1 is entered); then
+    #add "~" to beginning of car1
+    #car1pos++
+    
+  #if (number 2 is entered)
+    #add "~" to beginning of car2
+    #car2pos++
+    
+  #if (number 3 is entered)
+    #add "~" to behinning of car3
+    #car3pos++
+  
+  #clear screen
+    
+#if (car1pos=40)
+  #echo player 1 wins!
+#else if (car2pos=40)
+  #echo player 2 wins!
+#else if (car3pos=40)
+  #echo player 3 wins!
