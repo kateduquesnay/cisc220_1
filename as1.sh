e5c3a60dffa612e0ee34fd5844cf7ee487f8bee0
kateduquesnay <13khd@queensu.ca>
2016-10-17 17:35:12 -0400

    Update as1.sh

--- as1.sh
+++ as1.sh
@@ -5,3 +5,19 @@
 #Dean Wilkins-Reeves 10176758\
 # git clone https://github.com/kateduquesnay/cisc220_1.git cisc220_1\
 #Question 1\
+
+#Steps:
+#Ask for number --> Ask for operator --> Ask for number --> Produce result --Ask  for number 
+
+varNum = read -p "Please insert a number"
+
+#use while loop while variable =! q; program runs until user quits\
+
+while varNum =! q || varOp 
+do 
+  #Check to see if the variable is a number\
+  if ["$var" -eq "$Var"]
+  #Ask for an operator
+  varOp = read -p "Please insert operator"
+    
+    
