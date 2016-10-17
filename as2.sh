e37722f09d7ea09437a9047c6ff3411baa291933
14drwr <14drwr@queensu.ca>
2016-10-17 18:39:50 -0400

    Update as2.sh

--- as2.sh
+++ as2.sh
@@ -14,15 +14,15 @@ priv=" Private IP"
 #assigns local/private to the IP address if needed, then prints it
 function assign_locpriv()
 {
-  if [[ $1==127* ]]; #if string starts with 127
+  if [[ $1==127* ]] #if string starts with 127
   then
     out=$1$loc
     echo $out; #print with " Local IP" attached
   
-  elif [[ $1==10* || $1==192.168* ]]; #if string starts with 10 or 192.168
+  [ elif [[ $1==10* || $1==192.168* ]] #if string starts with 10 or 192.168
   then
    out=$1$priv
-   echo $out; #print with " Private IP" attached
+   echo $out; ]*#print with " Private IP" attached
   fi
 }
 
