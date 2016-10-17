3b37cc5ce441bea448ec3dd834de7818e71895a0
14drwr <14drwr@queensu.ca>
2016-10-17 17:54:07 -0400

    added comments, tried to clean up

--- as2.sh
+++ as2.sh
@@ -7,21 +7,25 @@
 # git clone https://github.com/kateduquesnay/cisc220_1.git cisc220_1\
 
 #Question 2\
-#Get ip address\
+#These variables used for easy concatenation later
 loc=" Local IP"
 priv=" Private IP"
 
+#Get the IP addresses on separate lines
 addresses=$(ifconfig | grep 'inet addr:' | awk '{print $2}' | awk -F":" '{print $2}')
 
+#iterate through addresses variable and run assign_locpriv on each line
 while read line; do assign_locpriv $line; done <<< "$addresses"
 
+#assigns local/private to the IP address if needed, then prints it
 function assign_locpriv()
 {
   if [[ $1==127* ]] 
   then
-  echo $ip$local
+  echo $ip$local #print with " Local IP" attached
   [ elif [[ $1==10* || $1==192.168* ]] 
   then
-  echo $ip$priv]*
+  echo $ip$priv]* #print with " Private IP" attached
+  [ else ] #do nothing
   fi
 }
