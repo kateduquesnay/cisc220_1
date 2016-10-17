e84046b9596921994ac0667ecff859476271cfe2
kateduquesnay <13khd@queensu.ca>
2016-10-17 17:19:14 -0400

    Update as1.sh

--- as1.sh
+++ as1.sh
@@ -5,3 +5,9 @@
 #Dean Wilkins-Reeves 10176758\
 # git clone https://github.com/kateduquesnay/cisc220_1.git cisc220_1\
 #Question 1\
+
+
+#Use while loop; while variable =! q
+
+varNum = read -p "Please insert a number"
+varOp = read -p "Please insert an operator"
