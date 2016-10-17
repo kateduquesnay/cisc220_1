52d91fcd967b7aad8e21634e444376763acbd518
kateduquesnay <13khd@queensu.ca>
2016-10-17 16:54:43 -0400

    Update as2.sh

--- as2.sh
+++ as2.sh
@@ -1,3 +1,10 @@
+#Assignment 1\
+#Sarah Carter 10193595\
+#Katherine DuQuesnay 10138898\
+#Adele McCallum 10093867\
+#Dean Wilkins-Reeves 10176758\
+# git clone https://github.com/kateduquesnay/cisc220_1.git cisc220_1\
+
 #Question 2\
 #Get ip address\
 ifconfig | grep 'inet addr:' | awk '{print $2}' | awk -F":" '{print $2}'
