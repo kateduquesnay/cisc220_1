acdab2baabceadc14ccbed8629eb61cdc54a9deb
14drwr <14drwr@queensu.ca>
2016-10-17 18:50:09 -0400

    Update as2.sh

--- as2.sh
+++ as2.sh
@@ -18,7 +18,7 @@ function assign_locpriv()
     out=$1$loc
     echo $out; #print with " Local IP" attached
   
-  elif [[ $1== 10* || $1 == 192.168* ]]; then #if string starts with 10 or 192.168
+  elif [[ $1 == 10* || $1 == 192.168* ]]; then #if string starts with 10 or 192.168
    out=$1$priv
    echo $out; #print with " Private IP" attached
   fi
